513a61337020f2729bb69378c459f95d111ae1dd
guang1z <g814635276@163.com>
2020-11-28 11:14:58 +0800

    2020-11-28提交项目代码

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <common/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Common from './components/common/Common'
+export default {
+  name: 'App',
+  components: {
+    Common
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/common/Common.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="common">
+    <el-container>
+      <el-header>管院团委管理系统</el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>部门管理</span>
+              </template>
+              <el-menu-item index="1-1">媒体部</el-menu-item>
+              <el-menu-item index="1-2">秘书处</el-menu-item>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">信息通知</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-document"></i>
+              <span slot="title">物资管理</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-container>
+          <el-main>内容</el-main>
+          
+        </el-container>
+      </el-container>
+      <el-footer>基于vue和springboot的管理系统</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Common',
+
+}
+</script>
+
+<style scoped>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-footer {
+    font-size: 13px;
+  }
+
+  .el-aside {
+    /* background-color: #D3DCE6; */
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
